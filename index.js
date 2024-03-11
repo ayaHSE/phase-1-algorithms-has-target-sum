af701ef06ab870bcaadc5d1c53a0c0c4c04aa280
Aya Hassan <147654234+ayaHSE@users.noreply.github.com>
2024-03-11 21:51:44 +0300

    done

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const seen = new Set();
+
+  for (let num of array) {
+    const complement = target - num;
+    if (seen.has(complement)) {
+      return true;
+    }
+    seen.add(num);
+  }
+
+  return false;
 }
+
 
 /* 
   Write the Big O time complexity of your function here
